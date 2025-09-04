7ceba6dc3a62290581aa04c6bd64cfe0f45906cb
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-09-04 19:25:46 +0330

    Development of the main page of the site, part one

---
+++ src/components/template/HomePage.js
@@ -0,0 +1,13 @@
+function HomePage() {
+  return (
+    <div>
+      <img src="/images/Untitle.png" />
+      <p className="  font-semibold text-2xl !mr-100 !mt-6">
+        <span className="!text-[#28A745]">تورینو</span>
+        برگذار کننده  بهترین  تور  های  داخلی و خارجی </p>
+        
+    </div>
+  );
+}
+
+export default HomePage;
