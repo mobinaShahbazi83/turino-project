6d86bc36459cd9d92102eb4905c4eb73d8dfca3b
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-09-06 22:00:54 +0330

    Development of the main page of the site, part two

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -6,11 +6,11 @@ import Header from "./Header"
 
 function Layout({children}) {
   return (
-    <>
+    <div className="max-w-[1440px] m-auto ">
     <Header/>
     <div>{children}</div>
     <Footer/>
-    </>
+    </div>
   )
 }
 
--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -1,11 +1,42 @@
+import { FaLocationDot } from "react-icons/fa6";
+import { RiEarthFill } from "react-icons/ri";
+import { FaCalendarAlt } from "react-icons/fa";
+
 function HomePage() {
   return (
     <div>
       <img src="/images/Untitle.png" />
-      <p className="  font-semibold text-2xl !mr-100 !mt-6">
-        <span className="!text-[#28A745]">تورینو</span>
-        برگذار کننده  بهترین  تور  های  داخلی و خارجی </p>
-        
+      <p className="  text-2xl flex justify-center !mt-6  font-normal ">
+        <span className="!text-[#28A745] mx-2 "> تورینو </span>
+        برگذار کننده بهترین تور های داخلی و خارجی
+      </p>
+      <div className="flex justify-between items-center mt-8 border border-[#00000029] rounded-xl !px-2 !py-4 gap-2 w-[750px] h-[67px] m-auto ">
+        <div className="flex gap-0.5">
+          <FaLocationDot className="mt-2 mb-2" />
+          <select className=" appearance-none">
+            <option>مبدا</option>
+          </select>
+        </div>
+        |
+        <div className="flex gap-1">
+          <RiEarthFill className="mt-2 mb-2" />
+          <select className=" appearance-none">
+            <option>مقصد</option>
+          </select>
+        </div>
+        |
+        <div className="flex gap-1">
+          <FaCalendarAlt className="mt-2 mb-2" />
+          <select className=" appearance-none">
+            <option>تاریخ</option>
+          </select>
+        </div>
+        <div>
+          <button className=" border-[#28A745] text-xl text-[#FFFFFF] bg-[#28A745] rounded-4xl w-[130px] h-[40px]  ">
+            جستجو
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
